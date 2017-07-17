f6c7e39625bd2b3c07c969e326b4a263da334710
Pratheek Adidela <apratheek@unaxiom.com>
2017-07-17 23:42:26 +0530

    Code has been reorganized into class.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import actions
-import functions
-import tkinter
+# import actions
+# import functions
+# import tkinter
+import focusapp
 
 # -----------------------------------------------------
 # Check here if the block is still active
@@ -10,29 +11,35 @@ import tkinter
 # -----------------------------------------------------
 # Parent Window
 # Create the parent window
-parentWindow = functions.generateParentWindow(tkinter.Tk())
 
-# Bind the slider
-functions.generateSlider(parentWindow)
-# Bind the Start button
-functions.generateStartButton(parentWindow)
-# Bind the View button
-functions.generateViewButton(parentWindow)
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+# parentWindow = functions.generateParentWindow(tkinter.Tk())
 
-# -----------------------------------------------------
-# Listbox window
-# Create a window
-# Attach a listbox that displays all the sites
-# Attach an Entry widget that can accept text
-# Attach a ADD button 
+# # Bind the slider
+# functions.generateSlider(parentWindow)
+# # Bind the Start button
+# functions.generateStartButton(parentWindow)
+# # Bind the View button
+# functions.generateViewButton(parentWindow)
+
+# # -----------------------------------------------------
+# # Listbox window
+# # Create a window
+# # Attach a listbox that displays all the sites
+# # Attach an Entry widget that can accept text
+# # Attach a ADD button 
 
 
-# -----------------------------------------------------
-# Actions
-# Attach an event handler to the view button to display a listbox with all the sites
-# Attach an event handler to the start button that will take all the sites mentioned and rewrite the /etc/hosts file
-# Attach an event handler to the add button that will take the text in the entry widget and add it to the listbox
-# Attach a context menu to the listbox that will have a function to remove the item from the listbox
+# # -----------------------------------------------------
+# # Actions
+# # Attach an event handler to the view button to display a listbox with all the sites
+# # Attach an event handler to the start button that will take all the sites mentioned and rewrite the /etc/hosts file
+# # Attach an event handler to the add button that will take the text in the entry widget and add it to the listbox
+# # Attach a context menu to the listbox that will have a function to remove the item from the listbox
 
-# Start
-parentWindow.mainloop()
+# # Start
+# parentWindow.mainloop()
+
+# $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+a = focusapp.FocusApp()
+a.start()
